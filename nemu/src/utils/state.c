4360196d525b248f31e27f2fce447db9b5f84baf
tracer-ics2025 <tracer@njuics.org>
2025-12-25 16:26:04 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:26:04 up  2:13,  1 user,  load average: 0.00, 0.00, 0.00

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -22,6 +22,5 @@ int is_exit_status_bad()
 {
   int good = (nemu_state.state == NEMU_END && nemu_state.halt_ret == 0) ||
              (nemu_state.state == NEMU_QUIT);
-  printf("DEBUG: state=%d, QUIT=%d, good=%d\n", nemu_state.state, NEMU_QUIT, good);
   return !good;
 }

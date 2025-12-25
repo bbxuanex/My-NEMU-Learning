1bc4d23f9dc72bf83642a2b44038849c34753af5
tracer-ics2025 <tracer@njuics.org>
2025-12-25 17:11:28 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:11:28 up  2:58,  1 user,  load average: 0.07, 0.04, 0.01

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -53,6 +53,13 @@ static int cmd_q(char *args) {
   return -1;
 }
 
+static int cmd_si(char*args)
+{
+  cpu_exec(1);//只执行一条命令
+  return 0;
+}
+
+
 static int cmd_help(char *args);
 
 static struct {
@@ -63,6 +70,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  {"si","Step one instruction",cmd_si},
 
   /* TODO: Add more commands */
 

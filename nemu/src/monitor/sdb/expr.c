57c5bf48e47a2bb2ca951628b1e8963fb64e091f
tracer-ics2025 <tracer@njuics.org>
2025-12-27 18:21:26 +0800

    >  compile NEMU 231220000 张三 Linux shuimushi-virtual-machine 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:26 up 10:35,  1 user,  load average: 0.00, 0.02, 0.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -116,6 +116,9 @@ static bool make_token(char *e) {
           }
           strncpy(tokens[nr_token].str, substr_start, substr_len);
           tokens[nr_token].str[substr_len] = '\0';
+          tokens[nr_token].type = rules[i].token_type; // 设置类型
+          nr_token++;                                  // 记得这里也要加
+          break;                                       // ❌ 加上 break，不要穿透到 default
         default:
           tokens[nr_token].type = rules[i].token_type;
           nr_token++;
